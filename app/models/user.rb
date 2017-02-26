09745a266c17a318e8452185f50a14f11739bd82
Joe Marriott <marriott90@gmail.com>
2017-02-26 15:24:49 +0000

    added name field in sign up form - required

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+  validates_uniqueness_of :email
+  validates_presence_of :name
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -16,7 +19,7 @@ class User < ApplicationRecord
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        uder.email = data["email"] if user.email.blank?
+        user.email = data["email"] if user.email.blank?
       end
     end
   end
